de020a23aa197ce9dc559191ca23432a6318f69b
rajasekhar2304 <raajasekharr@gmail.com>
2022-08-05 03:07:43 -0700

    written script1.sh

---
+++ script1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+for i in {1..5}
+do
+echo $i
+sleep 1
+done
+echo "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"
+for i in {6..10}
+do
+echo $i
+sleep 1
+done
+echo "The End"
